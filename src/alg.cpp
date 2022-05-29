00c2cb60084d6386a8c11c6a2f43360382040940
vdovinivan <99171081+vdovinivan@users.noreply.github.com>
2022-05-30 00:41:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,28 @@
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
+#include <string>
 #include  "bst.h"
 
-BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+BST<std::string> makeTree(const char* name) {
+      BST<std::string> tree;
+    std::ifstream file(name);
+    if (!file) {
+        std::cout << "Something wrong" <<"\n";
+        throw std::string("Can not open file!");
+    }
+    std::string word = "";
+    while (!file.eof()) {
+        char IdontWantToDoIt = file.get();
+        int ch = IdontWantToDoIt;
+        if ((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122)) {
+            word += tolower(ch);
+        } else { if (word != "") {
+                tree.Add(word);
+                word = "";
+            }
+        }
+    }
+    file.close();
+    return tree;
 }
